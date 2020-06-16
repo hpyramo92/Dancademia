0b3a5599e97e0b9f982e3660b357cef628ec5452
Henrique Pyramo <hpyramo92@gmail.com>
2020-06-16 09:17:01 -0300

    Pasta Service + controller

---
+++ backend/src/controllers/UsuarioController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class UsuarioController {
+    async create(request: Request, response: Response) {
+
+    }
+}
+
+export default UsuarioController;
